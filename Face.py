e96c79f1a04137f41b6a1c0cfa5932f20f3bf0f0
sagarjamadar399 <sagar.jamadar23@pcu.edu.in>
2025-03-16 20:59:05 +0530

    initial commit

--- Face.py
+++ Face.py
@@ -13,7 +13,7 @@ FaceDetectorResult = mp.tasks.vision.FaceDetectorResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 # Create a face detector instance with the live stream mode:
-def print_result(result: FaceDetectorResult, output_image: mp.Image, timestamp_ms: int):
+def print_result(result = FaceDetectorResult, output_image = mp.Image, timestamp_ms = int):
     print('face detector result: {}'.format(result))
 
 options = FaceDetectorOptions(
@@ -43,6 +43,7 @@ with FaceDetector.create_from_options(options) as detector:
         detector.detect_async(mp_image, frame_timestamp_ms)
 
         # Show frame with detection result
+        frame = cv2.flip(frame,1)
         cv2.imshow("Object Detection", frame)
 
         # Break the loop if 'q' is pressed
--- main.py
+++ main.py
@@ -1,93 +1,214 @@
-import cv2
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-import time 
-import os
+import cv2 
+import mediapipe as mp  
+import pyautogui
+import numpy as np
+import time
+from pynput.mouse import Button, Controller
+mouse = Controller()
 
-
-model_path = "gesture_recognizer.task"
-
-if not os.path.exists(model_path):
-    print(f"Error: Model path does not exist: {model_path}")
-else:
-    print(f"Model found at: {model_path}")
-
-
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
-
-BaseOptions = mp.tasks.BaseOptions
-GestureRecognizer = mp.tasks.vision.GestureRecognizer
-GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
-GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult 
-VisionRunningMode = mp.tasks.vision.RunningMode
-
-def print_result(result = GestureRecognizerResult, output_image = mp.Image, timestamp_ms =int):
-
-    try:
-        if result.gestures:
-            for gesture in result.gestures:
-                print(f"Detected Gesture: {gesture}")  
-                
-                if isinstance(gesture, list):
-                    for category in gesture:
-                        print(f"Gesture Category: {category.display_name}, Score: {category.score}")
-                else:
-                    print(f"Gesture does not have 'name' attribute. Structure: {gesture}")
-        
-        else:
-            print("No gesture detected.")
-        
-        if result.hand_landmarks:
-            for landmarks in result.hand_landmarks:
-                print("Hand landmarks detected.")
-                mp_drawing.draw_landmarks(output_image, landmarks, mp_hands.HAND_CONNECTIONS)
-
-                # for lm in landmarks.landmark:
-                    # landmark_list.append((lm.x, lm.y))
-        
-    except Exception as e:
-        print(f"Error in result callback: {e}")
-
-        
-
-options = GestureRecognizerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result
+screen_width,screen_height = pyautogui.size()
+mouse = Controller()
+mpHands = mp.solutions.hands
+scale = (2/1) # (mouse/hand)
+hands = mpHands.Hands(
+    static_image_mode=False,
+    model_complexity=1,
+    min_detection_confidence=0.9,
+    min_tracking_confidence=0.9,
+    max_num_hands=1
 )
 
+is_dragging = False
+last_click_time = 0  # Initialize last click time
+click_cooldown = 1  # Cooldown period for clicks (in seconds)
 
-with GestureRecognizer.create_from_options(options) as recognizer:
-   
+
+def get_angle(a,b,c):
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(np.degrees(radians))
+    return angle
+
+
+def get_distance(landmarks_list):
+    if len(landmarks_list) < 2:
+        return
+
+    (x1,y1),(x2,y2) = landmarks_list[0],landmarks_list[1]
+    L = np.hypot(x2 - x1, y2 -y1)
+    return np.interp(L,[0,1],[0,1000])   
+
+def find_finger_tip(processed):
+    if processed.multi_hand_landmarks:
+        hand_landmarks = processed.multi_hand_landmarks[0]
+        return hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP]
+  
+    return None
+
+
+def move_mouse(index_finger_tip):
+    if index_finger_tip is not None:
+        # frame_center_x = screen_width / 2
+
+        x = int(index_finger_tip.x * screen_width * scale)
+        y = int(index_finger_tip.y * screen_height * scale)
+
+        # x = int(frame_center_x + (x - frame_center_x)) 
+
+        mouse.position = (x, y)
+
+
+def is_left_click(landmarks_list,thumb_index_dist):
+    return (get_angle(landmarks_list[5],landmarks_list[6],landmarks_list[8] )< 50 and
+            get_angle(landmarks_list[9],landmarks_list[10],landmarks_list[12]) > 90 and 
+            thumb_index_dist > 150
+            )
+
+
+
+def is_right_click(landmarks_list,thumb_index_dist):
+    return(
+        get_angle(landmarks_list[5],landmarks_list[6],landmarks_list[8]) < 50 and
+        get_angle(landmarks_list[9],landmarks_list[10],landmarks_list[12]) < 50 and
+        thumb_index_dist > 150
+
+    )
+
+
+
+
+def is_double_click(landmarks_list,thumb_index_dist):
+    return(
+        get_angle(landmarks_list[5],landmarks_list[6],landmarks_list[8]) < 50 and
+        get_angle(landmarks_list[9],landmarks_list[10],landmarks_list[12]) < 90 and
+        thumb_index_dist > 150
+    )
+
+
+def is_screenshot(landmarks_list , thumb_index_dist):
+    return(
+        get_angle(landmarks_list[5],landmarks_list[6],landmarks_list[8]) < 50 and 
+        get_angle(landmarks_list[9],landmarks_list[10],landmarks_list[12]) < 50 and 
+        thumb_index_dist < 50     
+    )
+
+def handle_drag_and_drop(landmarks_list, processed):
+    global is_dragging
+    thumb_index_dist = get_distance([landmarks_list[4], landmarks_list[8]])
+    pinky_dist = get_distance([landmarks_list[16], landmarks_list[4]])  
+    middle_dist = get_distance([landmarks_list[12], landmarks_list[4]])  
+    ring_dist = get_distance([landmarks_list[20], landmarks_list[4]]) 
+
+    if thumb_index_dist < 30 and not is_dragging:
+        if pinky_dist > 50 and middle_dist > 50 and ring_dist > 50:
+            print("Pinch detected: Start dragging")
+            mouse.press(Button.left)  
+            is_dragging = True
+
+    if thumb_index_dist > 80 and is_dragging:  
+        print("Pinch released: Stop dragging")
+        mouse.release(Button.left) 
+        is_dragging = False
+
+    if is_dragging:
+        index_finger_tip = find_finger_tip(processed)
+        move_mouse(index_finger_tip)
+
+def detect_gestures(frame,landmarks_list,processed):
+    global last_click_time
+    if len(landmarks_list)>=21:
+
+
+        index_finger_tip = find_finger_tip(processed) 
+        # print(index_finger_tip)
+        thumb_index_dist = get_distance([landmarks_list[4], landmarks_list[5]])
+
+        handle_drag_and_drop(landmarks_list, processed)
+        
+        if thumb_index_dist < 50 and get_angle(landmarks_list[5],landmarks_list[6],landmarks_list[8])>90:
+            move_mouse(index_finger_tip)
+
+        #LEFT CLICK
+        elif is_left_click(landmarks_list, thumb_index_dist):
+            current_time = time.time()
+            if current_time - last_click_time >= click_cooldown:
+                mouse.press(Button.left)
+                mouse.release(Button.left)
+                last_click_time = current_time  # Update the last click time
+            cv2.putText(frame, "Left Click", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # RIGHT CLICK
+        elif is_right_click(landmarks_list, thumb_index_dist):
+            current_time = time.time()
+            if current_time - last_click_time >= click_cooldown:
+                mouse.press(Button.right)
+                mouse.release(Button.right)
+                last_click_time = current_time  # Update the last click time
+            cv2.putText(frame, "Right Click", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+
+
+       #DOUBLE CLICK
+        elif is_double_click(landmarks_list,thumb_index_dist):
+            mouse.click(Button.left,2)
+            cv2.putText(frame,"Double  Click", (50, 150),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+
+
+       #SCREENSHOT
+        elif is_screenshot(landmarks_list,thumb_index_dist):
+            timestamp = int(time.time())
+            screenshot_filename = f"screenshots/screenshot_{timestamp}.png"
+            pyautogui.screenshot(screenshot_filename)
+            cv2.putText(frame, "Screenshot Taken", (50, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2) 
+        
+        # This line Shows the distance between the thumbtip and indextip
+        # print(thumb_index_dist)
+
+def main():
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) 
+    draw = mp.solutions.drawing_utils
 
+    try:
+            while cap.isOpened():
+                ret, frame = cap.read()
 
-    if not cap.isOpened():
-        print("Error: Could not open webcam.")
-        exit()
+                if not ret:
+                    break
+                
+                start_time = time.time() #latencycheck
 
-    while True:
-        
-        ret, frame = cap.read()
-        
-        if not ret:
-            print("Error: Failed to grab frame.")
-            break
-       
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-        frame_timestamp_ms = int(time.time() * 1000)  
-        recognizer.recognize_async(mp_image, frame_timestamp_ms)
+                frame = cv2.flip(frame, 1)
+                frame_resized = cv2.resize(frame, (640, 480))  
+                frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                processed = hands.process(frameRGB)
 
-        cv2.imshow("Screen", frame)
+                landmarks_list = []
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        
-    cap.release()
-    cv2.destroyAllWindows()
+                if processed.multi_hand_landmarks:
+                    for hand_landmarks in processed.multi_hand_landmarks:  
+                        draw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
 
-    
+                        for lm in hand_landmarks.landmark:
+                            landmarks_list.append((lm.x, lm.y))  
+
+                    detect_gestures(frame,landmarks_list,processed)            
+                
+                # Enable this part for displaying Processing time
+                """
+                end_time = time.time() 
+                latency = end_time - start_time 
+                print(f"Processing time per frame: {latency:.4f} seconds")
+                print(f"{latency:.4f}")
+                """
+
+                cv2.imshow('Frame', frame)
+
+                if cv2.waitKey(1) & 0xFF == ord('q'): 
+                    break
+
+    finally:
+            cap.release()
+            cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
